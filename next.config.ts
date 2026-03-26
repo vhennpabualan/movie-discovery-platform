ede9f96fcd2a1efc9e4a1600a4dc4d5d4133739a
vhennpabualan <pabualanvhenn@gmai.com>
2026-03-26 13:23:28 +0800

    Add vsembed.ru to CSP frame-src directive for nested Vidsrc iframes

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-src 'self' https://vidsrc-embed.ru https://vidsrc-embed.su https://vidsrcme.su https://vsrc.su;",
+            value: "frame-src 'self' https://vidsrc-embed.ru https://vidsrc-embed.su https://vidsrcme.su https://vsrc.su https://vsembed.ru;",
           },
           {
             key: "X-Frame-Options",
@@ -40,7 +40,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-src 'self' https://vidsrc-embed.ru https://vidsrc-embed.su https://vidsrcme.su https://vsrc.su;",
+            value: "frame-src 'self' https://vidsrc-embed.ru https://vidsrc-embed.su https://vidsrcme.su https://vsrc.su https://vsembed.ru;",
           },
           {
             key: "X-Frame-Options",
